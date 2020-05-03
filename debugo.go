e5895bfeb7bcc1be9fa72653aea36b891a7e3a5a
Alexandre NEGREL <negrel.dev@protonmail.com>
2020-05-03 20:21:57 +0200

    adding default value -> no invalid memory address

--- debugo.go
+++ debugo.go
@@ -2,8 +2,8 @@ package debugo
 
 // Assert check that the given bool is true
 // else it will panic.
-var Assert func(bool, string) 
+var Assert func(bool, string) = func(_ bool, _ string) {}
 
 // AssertF check that the given function
 // return true else it will panic.
-var AssertF func(func() bool, string)
+var AssertF func(bool, string) = func(_ func() bool, _ string) {}
