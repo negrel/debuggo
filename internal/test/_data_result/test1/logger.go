0dd9cb8bfff1955cc039d3238135ad2185b31f22
Alexandre Negrel <negrel.dev@protonmail.com>
2020-08-06 18:27:15 +0200

    Fixing build tags generation

--- internal/test/_data_result/test1/logger.go
+++ internal/test/_data_result/test1/logger.go
@@ -7,11 +7,14 @@ package test1
 import (
 	// fmt is used in function parameters.
 	"fmt"
+	// Log package is only used in function body,
+	// so it should be removed by debuggo.
+	"log"
 )
 
 // Println calls Output to print to the standard
 // logger. Arguments are handled in the manner of
 // fmt.Println.
-func Println(_ ...fmt.Stringer) {
-
+func Println(a ...fmt.Stringer) {
+	log.Println(a)
 }
