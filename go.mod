9ffe6ee06457d4194f4be68c7ee2530858c48e78
Alexandre Negrel <negrel.dev@protonmail.com>
2020-08-08 22:20:17 +0200

    updating dependencies & improving generator test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/negrel/debuggo
 go 1.14
 
 require (
+	github.com/google/go-cmp v0.5.1
 	github.com/urfave/cli v1.22.4
 	golang.org/x/tools v0.0.0-20200717024301-6ddee64345a6
 )
--- internal/test/generator_test.go
+++ internal/test/generator_test.go
@@ -1,21 +1,75 @@
 package test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/negrel/debuggo/internal/generator"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 const (
-	dataDir    string = "./_data/src"
-	debuggoDir string = "./result"
-	resultDir  string = "./_data_result"
+	dataDir   string = "./_data/src"
+	outputDir string = "./output"
+	resultDir string = "./_data_result"
 )
 
+type testPkg struct {
+	path    string
+	goFiles map[string][]byte
+}
+
+func compareExpectedAndGeneratedFiles() error {
+	generatedPkgs, err := generator.GetAllPackages(outputDir)
+	if err != nil {
+		return err
+	}
+
+	expectedPkgs, err := generator.GetAllPackages(resultDir)
+	if err != nil {
+		return err
+	}
+
+	for i := range generatedPkgs {
+		generatedPkg := generatedPkgs[i]
+		expectedPkg := expectedPkgs[i]
+
+		for j := range generatedPkg.GoFiles {
+			generatedFilePath := generatedPkg.GoFiles[j]
+			expectedFilePath := expectedPkg.GoFiles[j]
+
+			generatedFile, err := ioutil.ReadFile(generatedFilePath)
+			if err != nil {
+				return err
+			}
+
+			expectedFile, err := ioutil.ReadFile(expectedFilePath)
+			if err != nil {
+				return nil
+			}
+
+			if diff := cmp.Diff(generatedFile, expectedFile); diff != "" {
+				header := fmt.Sprintf("\nGENERATED FILE: %v\nEXPECTED FILE: %v", generatedFilePath, expectedFilePath)
+				return fmt.Errorf("%v\nErrors generated file is different from the expected result:\n%s", header, diff)
+			}
+		}
+	}
+
+	return nil
+}
+
 func TestDataSet(t *testing.T) {
+	err := os.RemoveAll(outputDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	gen, err := generator.New(
 		generator.SrcDir(dataDir),
-		generator.OutputDir(debuggoDir),
+		generator.OutputDir(outputDir),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -23,6 +77,11 @@ func TestDataSet(t *testing.T) {
 
 	gen.Start()
 	if err := gen.Error(); err != nil {
+		t.Fatal(err)
+	}
+
+	err = compareExpectedAndGeneratedFiles()
+	if err != nil {
 		t.Fatal(err)
 	}
 }
