e612dd0490f66a71d8b2cddadf980ac31c9adbf2
Alexandre Negrel <negrel.dev@protonmail.com>
2020-08-11 23:40:04 +0200

    fixing ci gol lint script

--- ci/check_go_lint.sh
+++ ci/check_go_lint.sh
@@ -4,14 +4,17 @@
 set -u
 
 if ! command -v golint >/dev/null; then
-    echo "error: golint not found; go get -u golang.org/x/lint/golint" >&2
-    exit 1
+  echo "error: golint not found; go get -u golang.org/x/lint/golint" >&2
+  exit 1
 fi
 
-GOLINT_OUTPUT=$(IFS=$'\n'; golint ./... | grep -v "mockgen/internal/.*\|sample/.*")
+GOLINT_OUTPUT=$(
+  IFS=$'\n'
+  golint ./...
+)
 if [[ -n "${GOLINT_OUTPUT}" ]]; then
-    echo "${GOLINT_OUTPUT}"
-    echo
-    echo "The go source files aren't passing golint."
-    exit 1
+  echo "${GOLINT_OUTPUT}"
+  echo
+  echo "The go source files aren't passing golint."
+  exit 1
 fi
