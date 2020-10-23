810874deea21858250d30577dee57a49deb305db
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-24 00:34:43 +0200

    adding example for pkg/log

---
+++ examples/log/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/negrel/debuggo/pkg/log"
+)
+
+func main23() {
+	log.Traceln("[TRACE] - Trace log")
+	log.Debugln("[DEBUG] - Debug log")
+	log.Infoln("[INFO] - Info log")
+	log.Warnln("[WARN] - Warning log")
+	log.Errorln("[ERROR] - Error log")
+	log.Fatalln("[FATAL] - Fatal log")
+	// Will never be called because of log.Fatal
+	log.Panicln("[PANIC] - Panic log")
+}
